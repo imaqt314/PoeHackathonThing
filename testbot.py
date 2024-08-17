8209320a2ff150e51fbf423c31f5e87caf0a296a
imaqt314 <vyan.n.phan@gmail.com>
2024-08-17 13:57:03 -0700

    redo

--- testbot.py
+++ testbot.py
@@ -1,20 +1,9 @@
-"""
-
-Test bot for variety of functions.
-
-This is the simplest possible bot and a great place to start if you want to build your own bot.
-
-"""
-
 from __future__ import annotations
-
 from typing import AsyncIterable
-
-import fastapi_poe as fp
 from modal import App, Image, asgi_app, exit
+import fastapi_poe as fp
 
-
-class TestBot(fp.PoeBot):
+class GPT35TurboBot(fp.PoeBot):
     async def get_response(
         self, request: fp.QueryRequest
     ) -> AsyncIterable[fp.PartialResponse]:
@@ -27,16 +16,14 @@ class TestBot(fp.PoeBot):
     async def get_settings(self, setting: fp.SettingsRequest) -> fp.SettingsResponse:
         return fp.SettingsResponse(server_bot_dependencies={"Llama-3.1-8B-FW-128k": 1})
 
-
+      
 # =============== MODAL CODE TO RUN THE BOT ================== #
 REQUIREMENTS = ["fastapi-poe==0.0.47"]
 image = Image.debian_slim().pip_install(*REQUIREMENTS)
-app = App(name="testbot-poe", image=image)
+app = App(name="gpt35turbobot", image=image)
 
-
-@app.cls(image=image)
+@app.cls()
 class Model:
-    # See https://creator.poe.com/docs/quick-start#integrating-with-poe to find these values.
     def __init__(self):
         self.access_key = "dXiinSEYZOuhBXNVtOrh9Ie0I1dAsUH5"
         self.bot_name = "BotCJQT833Y2S"
@@ -45,27 +32,13 @@ class Model:
     def sync_settings(self):
         """Syncs bot settings on server shutdown."""
         if self.bot_name and self.access_key:
-            try:
-                fp.sync_bot_settings(self.bot_name, self.access_key)
-            except Exception:
-                print("\n*********** Warning ***********")
-                print(
-                    "Bot settings sync failed. For more information, see: https://creator.poe.com/docs/server-bots-functional-guides#updating-bot-settings"
-                )
-                print("\n*********** Warning ***********")
-
+            fp.sync_bot_settings(self.bot_name, self.access_key)
+            
     @asgi_app()
     def fastapi_app(self):
-        bot = TestBot()
-        if not self.access_key:
-            print(
-                "Warning: Running without an access key. Please remember to set it before production."
-            )
-            app = fp.make_app(bot, allow_without_key=True)
-        else:
-            app = fp.make_app(bot, access_key=self.access_key)
+        bot = GPT35TurboBot()
+        app = fp.make_app(bot, access_key=self.access_key)
         return app
-
 
 @app.local_entrypoint()
 def main():
